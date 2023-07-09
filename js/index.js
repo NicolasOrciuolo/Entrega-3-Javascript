c45bc51e010eff2e76e739e48bff15b9f9de4253
Nicolas Orciuolo <nicolas.orciuolo@gmail.com>
2023-07-09 02:16:19 -0300

    Se continúa trabajando el login

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-import {bbdd} from './bbdd.js'
-
-const login = document.querySelector("#login");
+import { bbdd } from './bbdd.js'
 
 const user = {
    email: '',
    contraseña: ''
 }
 
+const isAuth = JSON.parse(localStorage.getItem("isLogin")) || [];
+
 const inputs = document.querySelectorAll("input"); //Atrapo todos los elementos tipo input
 
 inputs.forEach((el) => {
    el.addEventListener("input", (e) => {
 
-      const { target } = e; 
-      const { name, value } = target; 
+      const { target } = e;
+      const { name, value } = target;
 
       if (name == "email") {
          user.email = value;
@@ -23,21 +23,25 @@ inputs.forEach((el) => {
          user.contraseña = value;
       }
    })
-}); 
-
+});
 
 const buttonLogin = document.getElementById("loginButton");
 
 buttonLogin.addEventListener("click", () => {
    const usuarioEncontrado = bbdd.find(el => el.usuario === user.usuario && el.contraseña === user.contraseña)
    if (usuarioEncontrado) {
-      console.log("USUARIO ENCONTRADOOOOOOOOOOOOO");
-      const auth = { name: usuarioEncontrado.usuario, isLogin: true }
+      const auth = { name: usuarioEncontrado.email, isLogin: true }
       localStorage.setItem("isLogin", JSON.stringify(auth));
    } else {
       console.log("No existe el usuario");
    }
 })
+
+if (isAuth.isLogin) {
+   window.location.replace = '../pag.html';
+   window.location.reload();
+}
+
 
 
 
